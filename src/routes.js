b9222fd345090aae53209f4cd7f369fa8836fac5
Jimmy Li <lihaoyuan1996@gmail.com>
2021-06-29 11:18:42 -0700

    update icon

--- src/routes.js
+++ src/routes.js
@@ -32,7 +32,7 @@ const routes = [
   {
     path: '/help',
     name: 'Help',
-    icon: 'nc-icon nc-bank',
+    icon: 'nc-icon nc-alert-circle-i',
     component: Help,
     layout: '/dashboard',
   },
